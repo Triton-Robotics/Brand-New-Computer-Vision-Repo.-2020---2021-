0a0b0f6f1ed4708f1f5a678bd35e6f32ad3e8101
Hongbin Miao <zslrmhb@126.com>
2021-11-13 16:24:20 -0800

    This will be Hongbin's branch

---
+++ cmake-build-debug/CMakeFiles/RM.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/zslrm/Desktop/Project/Brand-New-Computer-Vision-Repo.-2020---2021-/AutoAim/Armor/Aromor.cpp" "C:/Users/zslrm/Desktop/Project/Brand-New-Computer-Vision-Repo.-2020---2021-/cmake-build-debug/CMakeFiles/RM.dir/AutoAim/Armor/Aromor.cpp.obj"
+  "C:/Users/zslrm/Desktop/Project/Brand-New-Computer-Vision-Repo.-2020---2021-/AutoAim/Detector/Detector.cpp" "C:/Users/zslrm/Desktop/Project/Brand-New-Computer-Vision-Repo.-2020---2021-/cmake-build-debug/CMakeFiles/RM.dir/AutoAim/Detector/Detector.cpp.obj"
+  "C:/Users/zslrm/Desktop/Project/Brand-New-Computer-Vision-Repo.-2020---2021-/Camera/Camera_sdk.cpp" "C:/Users/zslrm/Desktop/Project/Brand-New-Computer-Vision-Repo.-2020---2021-/cmake-build-debug/CMakeFiles/RM.dir/Camera/Camera_sdk.cpp.obj"
+  "C:/Users/zslrm/Desktop/Project/Brand-New-Computer-Vision-Repo.-2020---2021-/PowerRune/PowerRuneDetector/PowerRuneDetector.cpp" "C:/Users/zslrm/Desktop/Project/Brand-New-Computer-Vision-Repo.-2020---2021-/cmake-build-debug/CMakeFiles/RM.dir/PowerRune/PowerRuneDetector/PowerRuneDetector.cpp.obj"
+  "C:/Users/zslrm/Desktop/Project/Brand-New-Computer-Vision-Repo.-2020---2021-/main.cpp" "C:/Users/zslrm/Desktop/Project/Brand-New-Computer-Vision-Repo.-2020---2021-/cmake-build-debug/CMakeFiles/RM.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/RM.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RM.dir/AutoAim/Armor/Aromor.cpp.obj"
+  "CMakeFiles/RM.dir/AutoAim/Detector/Detector.cpp.obj"
+  "CMakeFiles/RM.dir/Camera/Camera_sdk.cpp.obj"
+  "CMakeFiles/RM.dir/PowerRune/PowerRuneDetector/PowerRuneDetector.cpp.obj"
+  "CMakeFiles/RM.dir/main.cpp.obj"
+  "RM.exe"
+  "RM.exe.manifest"
+  "RM.pdb"
+  "libRM.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
